bb8d75a0208daa33c1508ad3eb994ba3901c7e7f
davidhartman00 <davidhartman00@gmail.com>
2019-05-07 13:13:44 -0700

    changed nav bar

--- nonfat_media/src/components/Navigation.js
+++ nonfat_media/src/components/Navigation.js
@@ -3,14 +3,34 @@ import React from 'react';
 const NavItem = props =>{
     const pageURI = window.location.pathname + window.location.search
     const liClassName = (props.path === pageURI) ? "nav-item active": "nav-item";
+    const aClassName = props.disabled ? "nav-link disabled": "nav-link";
     return(
         <li className={liClassName}>
-            <a href={props.path} className="nav-link" >
+            <a href={props.path} className={aClassName} >
             {props.name}
             {(props.path === pageURI) ? (<span className="sr-only">(current)</span>) : ""}
             </a>
         </li>
     );
+}
+
+class NavDropdown extends React.Component{
+    render(){
+        return(
+            <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                            </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="/">Action</a>
+                    <a className="dropdown-item" href="/">Another action</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="/">Something else here</a>
+                </div>
+            </li>
+
+        )
+    }
 }
 
 class Navigation extends React.Component{
@@ -24,12 +44,9 @@ class Navigation extends React.Component{
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Link</a>
-                        </li>
+                        <NavItem path="/" name="Home"/>
+                        <NavItem path="/page2" name="Page 2"/>
+                        <NavItem path="/page3" name="Disabled" disabled="true"/>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Dropdown
@@ -41,9 +58,9 @@ class Navigation extends React.Component{
                                 <a className="dropdown-item" href="/">Something else here</a>
                             </div>
                         </li>
-                        <li className="nav-item">
+                        {/* <li className="nav-item">
                             <a className="nav-link disabled" href="/">Disabled</a>
-                        </li>
+                        </li> */}
                     </ul>
                     <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
