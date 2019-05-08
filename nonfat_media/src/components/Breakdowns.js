d1c5da254db16f6155195ed1d9c95f465f47704f
davidhartman00 <davidhartman00@gmail.com>
2019-05-07 20:53:53 -0700

    added sort function in breakdowns display

--- nonfat_media/src/components/Breakdowns.js
+++ nonfat_media/src/components/Breakdowns.js
@@ -1,25 +1,36 @@
 import React, {Component} from 'react';
-// {
-// "added": 1485174060,
-// "title": "BELLEVILLE COP",
-// "type": "Feature Film",
-// "castingDirector": "Lori Wyman",
-// "startDate": 1492560000
-// }
+
 class Breakdowns extends Component{
+    timeConverter(time_recorded){
+        var a = new Date(time_recorded * 1000);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var time = month + ' ' + date + ' '  + year + ' ' + hour + ':' + min  ;
+    return time;
+    }
     render(){
-        const { breakdown } = this.props
-        const {breakdownList} = breakdown.map(breakdown =>{
+        const {breakdowns} = this.props;
+        const breakdownList = breakdowns.sort(function (a, b) { return b.added - a.added })
+            .map(breakdown => { 
             return (
-                <div className="breakdowns">
-                    <div>Title:</div>
-                    <div>Type:</div>
-                    <div>Added:</div>
-                    <div>Casting Director:</div>
-                    <div>Start Date:</div>
-                    <div>Select <input type="checkbox" /></div>
+                <div className="breakdown" key={breakdown.added}> {/*I don't like using a non-id value for a key, but for here this is the best given*/}
+                    <div>Title: {breakdown.title}</div>
+                    <div>Type: {breakdown.type}</div>
+                    <div>Added: {this.timeConverter(breakdown.added)}</div>
+                    <div>Casting Director: {breakdown.castingDirector}</div>
+                    <div>Start Date: {this.timeConverter(breakdown.startDate)}</div>
+                    <div>I'm Interested <input type="checkbox" /></div>
                 </div>
-            )}
+            )
+        })
+        return(
+            <div className="breakdown-list">
+                { breakdownList }
+            </div>
         )
     }
 }
